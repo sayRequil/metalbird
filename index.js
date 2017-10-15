65adae15d32a48b9ecaf8d218e42eaf984ec8559
Zack P <zack@zacklearns.com>
2017-10-15 12:25:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ var body = page.GetElementById('body');
 var managebar = new actionBar(header);
 var appOpen = new kernel();
 
-appOpen.onOpen((function(app)) {
+appOpen.onOpen(function(app)) {
   managebar.setTitle(app.Name);
   storage.appOpen = app.Name;
   body.innerHtml = new File(app.filePath).readIn
-}
+};
 
 appOpen.onClose((function(app)) {
   managebar.setTitle('Metalbird');
@@ -23,6 +23,6 @@ appOpen.onClose((function(app)) {
   var code = '';
   while (!index.eof) {
 		code += index.readln() + '\n';
-	};
+  };
   body.innerHtml = code;
 };
